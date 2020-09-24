27c86660b5fc30d62c27d52af65dc0d68c04078b
Алексей <aleksej@MacBook-Air-Aleksej.local>
2020-09-24 15:31:00 +0300

    changed div id

--- client/components/dashmain.js
+++ client/components/dashmain.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Dashmain = () => {
   return (
     <div>
-      <div id='Main'>Main </div>
+      <div id='title'>Main </div>
       <Link to='/dashboard/profile/f74f5c1b-b85f-4391-a411-40c557349987'>Go To Profile </Link>
       <Link to='/dashboard'>Go To Root </Link>
     </div>
